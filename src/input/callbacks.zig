bc483ed4fa9adb46e1ed7cbe9e5910f01c387727
foxnne <49629865+foxnne@users.noreply.github.com>
2022-08-23 16:16:13 -0500

    zoom: Clamp zooming.

--- src/input/callbacks.zig
+++ src/input/callbacks.zig
@@ -15,6 +15,9 @@ pub fn cursor(window: glfw.Window, x: f64, y: f64) void {
 pub fn scroll(_: glfw.Window, x: f64, y: f64) void {
     game.state.controls.mouse.scroll.x += @floatCast(f32, x);
     game.state.controls.mouse.scroll.y += @floatCast(f32, y);
+    game.state.controls.mouse.scroll.x = std.math.clamp(game.state.controls.mouse.scroll.x, 0.0, 100.0);
+    game.state.controls.mouse.scroll.y = std.math.clamp(game.state.controls.mouse.scroll.y, 0.0, 100.0);
+
 }
 
 pub fn key(_: glfw.Window, glfw_key: glfw.Key, _: i32, action: glfw.Action, _: glfw.Mods) void {
