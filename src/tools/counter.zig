55798c7d7c6c5db191e6cf4111651348329bd341
foxnne <foxnne@gmail.com>
2023-10-13 12:17:32 -0500

    counter: fix

--- src/tools/counter.zig
+++ src/tools/counter.zig
@@ -2,15 +2,14 @@ const std = @import("std");
 const game = @import("../aftersun.zig");
 
 pub const Counter = struct {
-    var value: u64 = 0;
+    value: u64 = 0,
 
-    pub fn count(self: Counter) u64 {
-        _ = self;
-        if (value == std.math.maxInt(u64)) {
-            value = 0;
+    pub fn count(self: *Counter) u64 {
+        if (self.value == std.math.maxInt(u64)) {
+            self.value = 0;
             std.log.debug("[{s}] Counter rolled over to 0, errors to be expected.", .{game.name});
         }
-        value += 1;
-        return value;
+        self.value += 1;
+        return self.value;
     }
 };
